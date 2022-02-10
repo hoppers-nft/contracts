f26b1bd2e13bf3648b12590f74a3da8d06a03159
joshieDo <ranriver@protonmail.com>
2022-02-10 17:51:49 +0400

    add market contract & tests

---
+++ src/Market.sol
@@ -0,0 +1,231 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.11;
+
+import {SafeTransferLib} from "@solmate/utils/SafeTransferLib.sol";
+import {ERC721} from "@solmate/tokens/ERC721.sol";
+
+contract Market {
+    using SafeTransferLib for address;
+
+    address public owner;
+    address public ownerCandidate;
+
+    address public immutable tokenAddress;
+
+    struct Listing {
+        uint256 id;
+        uint256 tokenId;
+        uint256 price;
+        address owner;
+        bool active;
+    }
+
+    mapping(uint256 => Listing) public listings;
+    uint256 public listingsLength;
+
+    /*///////////////////////////////////////////////////////////////
+                       MARKET MANAGEMENT SETTINGS
+    //////////////////////////////////////////////////////////////*/
+
+    uint256 public marketFee;
+    bool public isMarketOpen;
+    bool public emergencyDelisting;
+
+    /*///////////////////////////////////////////////////////////////
+                                  EVENTS
+    //////////////////////////////////////////////////////////////*/
+
+    event OwnerUpdated(address indexed newOwner);
+    event AddListingEv(
+        uint256 listingId,
+        uint256 indexed tokenId,
+        uint256 price
+    );
+    event UpdateListingEv(uint256 listingId, uint256 price);
+    event CancelListingEv(uint256 listingId);
+    event FulfillListingEv(uint256 listingId);
+
+    /*///////////////////////////////////////////////////////////////
+                                  ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    error Percentage0to100();
+    error ClosedMarket();
+    error InvalidListing();
+    error InactiveListing();
+    error InsufficientValue();
+    error InvalidOwner();
+    error OnlyEmergency();
+    error Unauthorized();
+
+    /*///////////////////////////////////////////////////////////////
+                    CONTRACT MANAGEMENT OPERATIONS
+    //////////////////////////////////////////////////////////////*/
+
+    constructor(address _tokenAddress, uint256 _marketFee) {
+        owner = msg.sender;
+
+        if (_marketFee > 100) {
+            revert Percentage0to100();
+        }
+
+        tokenAddress = _tokenAddress;
+        marketFee = _marketFee;
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != owner) revert Unauthorized();
+        _;
+    }
+
+    function setOwner(address _newOwner) external onlyOwner {
+        owner = _newOwner;
+        emit OwnerUpdated(_newOwner);
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                      MARKET MANAGEMENT OPERATIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function openMarket() external onlyOwner {
+        if (emergencyDelisting) {
+            emergencyDelisting = false;
+        }
+        isMarketOpen = true;
+    }
+
+    function closeMarket() external onlyOwner {
+        isMarketOpen = false;
+    }
+
+    function allowEmergencyDelisting() external onlyOwner {
+        emergencyDelisting = true;
+    }
+
+    function adjustFees(uint256 newMarketFee) external onlyOwner {
+        if (newMarketFee > 100) {
+            revert Percentage0to100();
+        }
+
+        marketFee = newMarketFee;
+    }
+
+    // If something goes wrong, we can close the market and enable emergencyDelisting
+    //    After that, anyone can delist active listings
+    //slither-disable-next-line calls-loop
+    function emergencyDelist(uint256[] calldata listingIDs) external {
+        if (!(emergencyDelisting && !isMarketOpen)) revert OnlyEmergency();
+
+        uint256 len = listingIDs.length;
+        //slither-disable-next-line uninitialized-local
+        for (uint256 i; i < len; ++i) {
+            uint256 id = listingIDs[i];
+            Listing memory listing = listings[id];
+            if (listing.active) {
+                listings[id].active = false;
+                ERC721(tokenAddress).transferFrom(
+                    address(this),
+                    listing.owner,
+                    listing.tokenId
+                );
+            }
+        }
+    }
+
+    function withdraw() external onlyOwner {
+        msg.sender.safeTransferETH(address(this).balance);
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                        LISTINGS WRITE OPERATIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function addListing(uint256 tokenId, uint256 price) external {
+        if (!isMarketOpen) revert ClosedMarket();
+
+        // overflow is unrealistic
+        unchecked {
+            uint256 id = listingsLength++;
+
+            listings[id] = Listing(id, tokenId, price, msg.sender, true);
+
+            emit AddListingEv(id, tokenId, price);
+
+            ERC721(tokenAddress).transferFrom(
+                msg.sender,
+                address(this),
+                tokenId
+            );
+        }
+    }
+
+    function updateListing(uint256 id, uint256 price) external {
+        if (!isMarketOpen) revert ClosedMarket();
+        if (id >= listingsLength) revert InvalidListing();
+        if (listings[id].owner != msg.sender) revert InvalidOwner();
+
+        listings[id].price = price;
+        emit UpdateListingEv(id, price);
+    }
+
+    function cancelListing(uint256 id) external {
+        if (id >= listingsLength) revert InvalidListing();
+
+        Listing memory listing = listings[id];
+
+        if (!listing.active) revert InactiveListing();
+        if (listing.owner != msg.sender) revert InvalidOwner();
+
+        listings[id].active = false;
+
+        emit CancelListingEv(id);
+
+        ERC721(tokenAddress).transferFrom(
+            address(this),
+            listing.owner,
+            listing.tokenId
+        );
+    }
+
+    function fulfillListing(uint256 id) external payable {
+        if (!isMarketOpen) revert ClosedMarket();
+        if (id >= listingsLength) revert InvalidListing();
+
+        Listing memory listing = listings[id];
+
+        if (!listing.active) revert InactiveListing();
+        if (msg.value < listing.price) revert InsufficientValue();
+        if (msg.sender == listing.owner) revert InvalidOwner();
+
+        listing.owner.safeTransferETH(
+            listing.price - (listing.price * marketFee) / 100
+        );
+
+        emit FulfillListingEv(id);
+
+        ERC721(tokenAddress).transferFrom(
+            address(this),
+            msg.sender,
+            listing.tokenId
+        );
+    }
+
+    function getListings(uint256 from, uint256 length)
+        external
+        view
+        returns (Listing[] memory listing)
+    {
+        unchecked {
+            uint256 numListings = listingsLength;
+            if (from + length > numListings) {
+                length = numListings - from;
+            }
+
+            Listing[] memory _listings = new Listing[](length);
+            //slither-disable-next-line uninitialized-local
+            for (uint256 i; i < length; ++i) {
+                _listings[i] = listings[from + i];
+            }
+            return _listings;
+        }
+    }}
---
+++ src/test/Market.t.sol
@@ -0,0 +1,243 @@
+import "ds-test/test.sol";
+
+import {BaseTest, HEVM, HopperNFT} from "./BaseTest.sol";
+
+import {Market} from "../Market.sol";
+
+contract HopperTest is BaseTest {
+
+    Market MARKET;
+    uint256 marketFee = 2;
+
+    function setUpMarket() public {
+
+        MARKET = new Market(
+            address(HOPPER),
+            marketFee
+        );
+
+        hevm.prank(user1);
+        HOPPER.setApprovalForAll(address(MARKET), true);
+        hevm.prank(user2);
+        HOPPER.setApprovalForAll(address(MARKET), true);
+
+        hevm.prank(user1);
+        hevm.expectRevert(abi.encodeWithSelector(Market.ClosedMarket.selector));
+        MARKET.addListing(6, 1 ether);
+
+        hevm.prank(user1);
+        hevm.expectRevert(abi.encodeWithSelector(Market.Unauthorized.selector));
+        MARKET.openMarket();
+
+        MARKET.openMarket();
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                       HELPER FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function updateListing(
+        address agent,
+        uint256 id,
+        uint256 amount
+    ) public {
+        hevm.prank(agent);
+        MARKET.updateListing(id, amount);
+    }
+
+    function addListing(
+        address agent,
+        uint256 tokenId,
+        uint256 amount
+    ) public {
+        hevm.prank(agent);
+        MARKET.addListing(tokenId, amount);
+        assert(HOPPER.ownerOf(tokenId) == address(MARKET));
+    }
+
+    function cancelListing(address agent, uint256 id) public {
+        Market.Listing memory listing = MARKET.getListings(id, 1)[0];
+        hevm.prank(agent);
+        MARKET.cancelListing(id);
+        assert(HOPPER.ownerOf(listing.tokenId) == listing.owner);
+    }
+
+    function fulfillListing(
+        address agent,
+        uint256 id,
+        uint256 amount
+    ) public {
+        Market.Listing memory listing = MARKET.getListings(id, 1)[0];
+        
+        uint256 prevAgentBalance = address(agent).balance;
+        uint256 prevOwnerBalance = address(listing.owner).balance;
+        uint256 prevMarketBalance = address(MARKET).balance;
+
+        hevm.prank(agent);
+        MARKET.fulfillListing{value: amount}(id);
+
+        assert(HOPPER.ownerOf(listing.tokenId) != listing.owner);
+        assert(HOPPER.ownerOf(listing.tokenId) == agent);
+
+        uint256 marketFee = (amount * marketFee) / 100;
+        assert(prevAgentBalance - amount == address(agent).balance);
+        assert(prevOwnerBalance + amount - marketFee == address(listing.owner).balance);
+        assert(prevMarketBalance + marketFee == address(MARKET).balance);
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                             TESTS
+    //////////////////////////////////////////////////////////////*/
+
+    function testMarketOwnership() public {
+        setUpMarket();
+
+        hevm.prank(user1);
+        hevm.expectRevert(abi.encodeWithSelector(Market.Unauthorized.selector));
+        MARKET.setOwner(user1);
+
+        MARKET.setOwner(user1);
+
+        assert(user1 == MARKET.owner());
+
+        hevm.prank(user1);
+        MARKET.setOwner(user2);
+
+        assert(user2 == MARKET.owner());
+    }
+
+    function testListings() public {
+        setUpMarket();
+
+        hevm.prank(user2, user2);
+        HOPPER.mint{value: MINT_COST*10}(10);
+        hevm.prank(user1, user1);
+        HOPPER.mint{value: MINT_COST*10}(10);
+
+        addListing(user2, 0, 1 ether);
+        addListing(user2, 1, 1 ether);
+        addListing(user2, 2, 1 ether);
+        addListing(user2, 4, 1 ether);
+        addListing(user2, 3, 0.01 ether);
+        addListing(user2, 5, 1 ether);
+
+        Market.Listing[] memory user2Listings = MARKET.getListings(3, 2);
+
+        assert(user2Listings[0].id == 3);
+        assert(user2Listings[0].owner == user2);
+        assert(user2Listings[0].tokenId == 4);
+        assert(user2Listings[0].price == 1 ether);
+
+        assert(user2Listings[1].id == 4);
+        assert(user2Listings[1].owner == user2);
+        assert(user2Listings[1].tokenId == 3);
+        assert(user2Listings[1].price == 0.01 ether);
+
+        // Invalid Owner Tests
+        hevm.prank(user1);
+        hevm.expectRevert(abi.encodeWithSelector(Market.InvalidOwner.selector));
+        MARKET.updateListing(0, 0 ether);
+
+        hevm.prank(user1);
+        hevm.expectRevert(abi.encodeWithSelector(Market.InvalidOwner.selector));
+        MARKET.cancelListing(0);
+
+        updateListing(user2, 1, 1 ether);
+        updateListing(user2, 2, 1 ether);
+        updateListing(user2, 3, 1 ether);
+
+        addListing(user1, 10, 1 ether);
+        addListing(user1, 11, 1 ether);
+        addListing(user1, 12, 1 ether);
+        addListing(user1, 13, 1 ether);
+        addListing(user1, 14, 1 ether);
+        addListing(user1, 15, 1 ether);
+
+        cancelListing(user2, 3);
+        cancelListing(user2, 5);
+        cancelListing(user2, 0);
+
+        fulfillListing(user1, 1, 1 ether);
+        fulfillListing(user2, 7, 1 ether);
+        fulfillListing(user2, 6, 1 ether);
+
+        cancelListing(user1, 9);
+        cancelListing(user1, 8);
+        cancelListing(user1, 11);
+        cancelListing(user1, 10);
+
+        // Withdrawing
+        uint256 prevMarketBalance = address(MARKET).balance;
+        uint256 prevOwner = address(user1).balance;
+
+        assert(prevMarketBalance != 0);
+
+        hevm.prank(user1);
+        hevm.expectRevert(abi.encodeWithSelector(Market.Unauthorized.selector));
+        MARKET.withdraw();
+
+        MARKET.setOwner(user1);
+
+        hevm.prank(user1);
+        MARKET.withdraw();
+
+        assert(prevMarketBalance + prevOwner == user1.balance);
+        assert(address(MARKET).balance == 0);
+
+    }
+
+    function testEmergency() public {
+
+        setUpMarket();
+
+        hevm.prank(user1, user1);
+        HOPPER.mint{value: MINT_COST*10}(10);
+
+        addListing(user1, 0, 1 ether);
+        addListing(user1, 1, 1 ether);
+        addListing(user1, 2, 1 ether);
+        addListing(user1, 3, 1 ether);
+
+        uint256[] memory arr = new uint256[](4);
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 2;
+        arr[3] = 3;
+
+        assertTrue(HOPPER.ownerOf(0) == address(MARKET));
+        hevm.prank(user2);
+        hevm.expectRevert(
+            abi.encodeWithSelector(Market.OnlyEmergency.selector)
+        );
+        MARKET.emergencyDelist(arr);
+
+        hevm.prank(user2);
+        hevm.expectRevert(abi.encodeWithSelector(Market.Unauthorized.selector));
+        MARKET.closeMarket();
+
+        hevm.prank(user2);
+        hevm.expectRevert(abi.encodeWithSelector(Market.Unauthorized.selector));
+        MARKET.allowEmergencyDelisting();
+
+        MARKET.closeMarket();
+        MARKET.allowEmergencyDelisting();
+
+        Market.Listing[] memory beforeListings = MARKET.getListings(0,4);
+        for(uint256 i; i< beforeListings.length; ++i) {
+            assert(beforeListings[i].active);
+        }
+        
+        // Anyone can delist them, but they go to the original owner.
+        hevm.prank(user2);
+        MARKET.emergencyDelist(arr);
+
+        Market.Listing[] memory listings = MARKET.getListings(0,4);
+        for(uint256 i; i< listings.length; ++i) {
+            assert(!listings[i].active);
+            assertTrue(HOPPER.ownerOf(listings[i].tokenId) == listings[i].owner);
+
+        }
+    }
+
+
+}
