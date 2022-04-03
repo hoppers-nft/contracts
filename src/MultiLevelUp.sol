9f6a18779931c5fe02b749a791e70fe910466584
kontrol-apa <grizzlyhillz@gmail.com>
2022-04-03 14:24:17 +0200

    added Multilevelup contract and its tests

---
+++ src/MultiLevelUp.sol
@@ -0,0 +1,112 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.12;
+
+import {SafeTransferLib} from "@solmate/utils/SafeTransferLib.sol";
+import {ERC721} from "@solmate/tokens/ERC721.sol";
+import {HopperNFT} from "./Hopper.sol";
+import {Fly} from "./Fly.sol";
+
+
+//slither-disable-next-line locked-ether
+contract MultilevelUp {
+    address public immutable FLY;
+    address public immutable HOPPER;
+    address public owner;
+
+    string public LEVEL_GAUGE_KEY;
+    uint public constant LEVEL_UP_COST_CATEGORY_0 = 1051288022900527273848688253688596547214916831956288517063648523173704499200;
+    uint public constant LEVEL_UP_COST_CATEGORY_1 = 4549695761791932346867726865992642700429225143980401255780434475327577129635;
+    uint public constant LEVEL_UP_COST_CATEGORY_2 = 11811543960987259923365662880439990815517098782391593173251212966463154752205;
+    uint public constant LEVEL_UP_COST_CATEGORY_3 = 23684932826573291205163703188724542792741345673977579048319878367674389961585;
+    uint public constant LEVEL_UP_COST_CATEGORY_4 = 40841274218494176753190239841356774803731824592103246109976028595510861837925;
+    uint public constant LEVEL_UP_COST_CATEGORY_5 = 63828299623757599652339988891003529005521872508617173962538202162096509771025;
+    uint public constant LEVEL_UP_COST_CATEGORY_6 = 754575304390847322886335;
+
+
+    event UpdatedOwner(address indexed owner);
+    error LevelOutofBounds();
+    error WrongTokenID();
+    error Unauthorized();
+
+    constructor(  address fly,address hopper
+    ) {
+        owner = msg.sender;
+        FLY = fly;
+        HOPPER = hopper;
+        LEVEL_GAUGE_KEY = "LEVEL_GAUGE_KEY";
+
+    }
+    modifier onlyOwner() {
+        if (msg.sender != owner) revert Unauthorized();
+        _;
+    }
+
+    function setOwner(address _owner) external onlyOwner {
+        owner = _owner;
+        emit UpdatedOwner(_owner);
+    }
+
+    function calculateMultiLevelUpCost(uint currentLevel, uint targetLevel) public view returns (uint) {
+        if(targetLevel > 100) { revert LevelOutofBounds(); }
+        return (getCumulativeLevelUpCost(targetLevel) - getCumulativeLevelUpCost(currentLevel)) * 10**17; 
+    }
+
+
+
+    function getValueFromBitMap(uint256 x, uint position) internal pure returns (uint) {
+        return (((x >> ((position) * 16)) & type(uint16).max));
+    }
+
+    function getCumulativeLevelUpCost(uint level) internal view returns  (uint) {
+
+        uint category = level / 16;
+        if (category == 0){
+            category = LEVEL_UP_COST_CATEGORY_0;
+        }
+        else if (category == 1){
+            category = LEVEL_UP_COST_CATEGORY_1;
+        }
+        else if (category == 2){
+            category = LEVEL_UP_COST_CATEGORY_2;
+        }
+        else if (category == 3){
+            category = LEVEL_UP_COST_CATEGORY_3;
+        }
+        else if (category == 4){
+            category = LEVEL_UP_COST_CATEGORY_4;
+        }
+        else if (category == 5){
+            category = LEVEL_UP_COST_CATEGORY_5;
+        }
+        else if (category == 6){
+            category = LEVEL_UP_COST_CATEGORY_6;
+        }    
+        uint position = level % 16;
+        return getValueFromBitMap(category,position);
+
+    }
+
+
+    function multiLevelUp(uint levels, uint tokenId) external {
+
+        HopperNFT IHOPPER = HopperNFT(HOPPER);
+        if (IHOPPER.ownerOf(tokenId) != msg.sender) {
+            revert WrongTokenID();
+        }
+
+        HopperNFT.Hopper memory hopper = IHOPPER.getHopper(tokenId);
+        uint currentLevel = hopper.level;
+        uint levelUpCost = calculateMultiLevelUpCost(currentLevel,currentLevel + levels);
+        Fly(FLY).burn(msg.sender, levelUpCost);
+        
+        
+        for(uint i ;i<levels; ++i){
+            IHOPPER.levelUp(tokenId);
+        }
+
+        // Reset Hopper internal gauge
+        IHOPPER.setData(LEVEL_GAUGE_KEY, tokenId, 0);
+
+    }
+
+}
---
+++ src/test/Multilevel.t.sol
@@ -0,0 +1,165 @@
+//SPDX-License-Identifier: UNLICENSED
+import "ds-test/test.sol";
+import "../MultiLevelUp.sol";
+import {BaseTest, HEVM, HopperNFT, Ballot} from "./BaseTest.sol";
+
+
+contract MultilevelTestsV2 is BaseTest {
+
+    address Bob = address(0x132323);
+    MultilevelUp public MULTILEVELUP;
+
+
+
+    function initialPreperation() internal {
+        MULTILEVELUP = new MultilevelUp(address(FLY),address(HOPPER));
+        address[] memory zones = new address[](1);
+        zones[0] = address(MULTILEVELUP);
+        address owner = HOPPER.owner();
+        hevm.prank(owner);
+        HOPPER.addZones(zones);
+        hevm.prank(owner);
+        FLY.addZones(zones);
+
+    }
+
+
+    function getHopperLevel(uint tokenId) internal view returns (uint) {
+        HopperNFT.Hopper memory hopper = HOPPER.getHopper(tokenId);
+        return hopper.level;
+
+    }
+
+    function mintHopperForUser(address user, uint tokenId) internal   {
+        hevm.prank(user);
+        HOPPER.addHopper(tokenId);
+    }
+
+    function mintHopperwithLevel(address user, uint tokenId, uint200 startLevel) public {
+        hevm.prank(user);
+        HOPPER.addHopperLevelwithLevel(tokenId, startLevel);
+    }
+
+    
+    function test1(uint levels) public {
+        if(levels > 98) {
+            return;
+        }
+        initialPreperation();
+        mintHopperForUser(Bob,12);
+        FLY.mockMint(Bob, 11512 ether);
+        uint initialBobBalance = FLY.balanceOf(Bob);
+        
+        hevm.prank(Bob);
+        uint currentLevel = getHopperLevel(12);
+
+        //naive level up 
+        for (uint256 index = 0; index <  levels; index++) {
+            hevm.prank(Bob);
+            POND.levelUp(12, false);
+        }
+        uint LevelUpCost =  initialBobBalance - FLY.balanceOf(Bob) ;
+        uint currentLevelAfter = getHopperLevel(12);
+        assertEq(currentLevel + levels, currentLevelAfter);
+        
+
+        mintHopperForUser(Bob,13);
+        uint initialBobBalanceBeforeMLU = FLY.balanceOf(Bob);
+
+        uint HopperLevelBeforeMLU = getHopperLevel(13);
+        hevm.prank(Bob);
+        MULTILEVELUP.multiLevelUp(levels, 13);
+        uint HopperLevelAfterMLU = getHopperLevel(13);
+
+
+        uint LevelUpCostMLU =  initialBobBalanceBeforeMLU - FLY.balanceOf(Bob) ;
+        assertEq(LevelUpCostMLU, LevelUpCost);
+        assertEq(HopperLevelBeforeMLU + levels, HopperLevelAfterMLU);
+        assertEq(HopperLevelAfterMLU, currentLevelAfter);
+
+
+        
+    }
+
+
+    function testFaillevel100() public {
+    
+        initialPreperation();
+        FLY.mockMint(Bob, 11512 ether);
+        mintHopperwithLevel(Bob,13,100);
+        assertEq(getHopperLevel(13),100);
+        hevm.prank(Bob);
+        MULTILEVELUP.multiLevelUp(1, 13);
+
+    
+    }
+
+    function testMultiLevelAfterRebirth() public {
+    
+        initialPreperation();
+        FLY.mockMint(Bob, 11512 ether);
+        mintHopperwithLevel(Bob,13,1);
+        hevm.prank(Bob);
+        MULTILEVELUP.multiLevelUp(99, 13);
+        assertEq(getHopperLevel(13),100);
+
+        hevm.prank(Bob);
+        HOPPER.rebirth(13);
+        assertEq(getHopperLevel(13),1);
+        
+        hevm.prank(Bob);
+        MULTILEVELUP.multiLevelUp(17, 13);
+        assertEq(getHopperLevel(13),18);
+
+    
+    }
+
+
+
+    function testAlllevels() public {
+        initialPreperation();
+        uint tokenId = 1;
+        
+        FLY.mockMint(Bob, 11511111112 ether);
+
+        for (uint200 startLevel = 1; startLevel < 100; startLevel++){
+            for (uint256 levels = 1; levels < (100 - startLevel); levels++) {  
+          
+            mintHopperwithLevel(Bob,tokenId,startLevel);
+            uint initialBobBalance = FLY.balanceOf(Bob);
+            uint currentLevel = getHopperLevel(tokenId);
+    
+            //naive level up 
+            for (uint256 x = 0; x <  levels; x++) {
+                hevm.prank(Bob);
+                POND.levelUp(tokenId, false);
+            }
+
+            uint naiveLevelUpCost =  initialBobBalance - FLY.balanceOf(Bob) ;
+            uint currentLevelAfterNaiveLevelUp = getHopperLevel(tokenId);
+            assertEq(currentLevel + levels, currentLevelAfterNaiveLevelUp);
+            
+            //mint another token
+            tokenId++;
+            mintHopperwithLevel(Bob,tokenId,startLevel);
+            uint BobBalanceBeforeMulti = FLY.balanceOf(Bob);
+    
+            uint HopperLevelBeforeMulti = getHopperLevel(tokenId);
+            hevm.prank(Bob);
+            MULTILEVELUP.multiLevelUp(levels, tokenId);
+            uint HopperLevelAfterMLU = getHopperLevel(tokenId);
+    
+    
+            uint LevelUpCostMulti =  BobBalanceBeforeMulti - FLY.balanceOf(Bob) ;
+            assertEq(LevelUpCostMulti, naiveLevelUpCost);
+            assertEq(HopperLevelBeforeMulti + levels, HopperLevelAfterMLU);
+            assertEq(HopperLevelBeforeMulti, currentLevelAfterNaiveLevelUp);
+            tokenId++;
+            }
+
+          }    
+    }      
+
+
+
+}
